180de6fe83d7078e7ae02e1c67a074829f9bc042
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-12-29 15:49:33 +0100

    5-test-suites-and-specifications

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,20 @@
+describe('CalculatorService', () => {
+
+  // To test with hot reloading, use: ng test
+  // To test once without hot reloading, use: ng test --no-watch
+
+  it('should add two numbers', () => {
+
+    // Not yet ready, use pending
+    pending();
+
+  });
+
+  it('should substract two numbers', () => {
+
+    // Fail programatically
+    fail();
+
+  });
+
+});
