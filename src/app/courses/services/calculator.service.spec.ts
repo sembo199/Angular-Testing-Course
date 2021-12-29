aeaf1979daee9b0e84a33d140bd2fa287edbbb3d
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-12-29 16:16:22 +0100

    8-test-setup-using-beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -6,7 +6,20 @@ describe('CalculatorService', () => {
   // To test with hot reloading, use: ng test
   // To test once without hot reloading, use: ng test --no-watch
 
+  let calculator: CalculatorService,
+  loggerSpy: any;
+
+  // Initialization logic before each spec
+  beforeEach(() => {
+    console.log("Calling beforeEach");
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    // Setting this here means the object is reset every test so tests can not interfere eachother
+    calculator = new CalculatorService(loggerSpy);
+  });
+
   it('should add two numbers', () => {
+
+    console.log("Add Test");
 
     // Not yet ready, use pending
     //pending();
@@ -23,10 +36,11 @@ describe('CalculatorService', () => {
      *  You should use the following instead
      */
 
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+    // const logger = jasmine.createSpyObj('LoggerService', ['log']);
 
-    // Setup phase, prepare the test with the things you need
-    const calculator = new CalculatorService(logger);
+    // // Setup phase, prepare the test with the things you need
+    // // This is now repeated for every spec, this should be handled elsewhere.
+    // const calculator = new CalculatorService(logger);
 
     // Execution phase, trigger the operation we want to test
     const result = calculator.add(2,2);
@@ -34,23 +48,30 @@ describe('CalculatorService', () => {
     // Assertion phase, check the results
     expect(result).toBe(4);
 
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
   });
 
   it('should substract two numbers', () => {
 
+    console.log("Subtract Test");
+
     // Fail programatically
     // fail();
 
-    // Setup phase, prepare the test with the things you need
-    const calculator = new CalculatorService(new LoggerService());
+    // // Setup phase, prepare the test with the things you need
+    // const logger = jasmine.createSpyObj('LoggerService', ['log']);
+
+    // // Setup phase, prepare the test with the things you need
+    // const calculator = new CalculatorService(logger);
 
     // Execution phase, trigger the operation we want to test
     const result = calculator.subtract(2,2);
 
     // Assertion phase, check the results
     expect(result).toBe(0, "unexpteced substraction result");
+
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
   });
 
