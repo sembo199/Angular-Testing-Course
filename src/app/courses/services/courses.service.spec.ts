155bf638047b5275f0c44322d3cd026c4cf56e12
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2022-01-05 14:21:27 +0100

    12-http-service-testing

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from "@angular/core/testing";
 import { CoursesService } from "./courses.service";
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
+import { assert } from "console";
+import { COURSES } from "../../../../server/db-data";
 
 describe("CoursesService", () => {
 
@@ -22,7 +24,18 @@ describe("CoursesService", () => {
   });
 
   it('should retrieve all courses', () => {
+    coursesService.findAllCourses().subscribe(courses => {
+      expect(courses).toBeTruthy('No courses returned');
+      expect(courses.length).toBe(12, 'Incorrect number of courses');
+      const course = courses.find(course => course.id === 12);
+      expect(course.titles.description).toBe('Angular Testing Course');
+    });
 
+    const req = httpTestingController.expectOne('/api/courses');
+    expect(req.request.method).toEqual('GET');
+    req.flush({
+      payload: Object.values(COURSES)
+    });
   });
 
 });
