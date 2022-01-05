e043615e5a05acbe2c12dedf312a92bd3e6b0339
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2022-01-05 14:34:03 +0100

    13-prevent-unintended-requests-using-HttpTestingController

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -38,4 +38,23 @@ describe("CoursesService", () => {
     });
   });
 
+  it('should retrieve one course', () => {
+    coursesService.findCourseById(12).subscribe(course => {
+      expect(course).toBeTruthy('No course found with ID 12');
+      expect(course.id).toBe(12);
+    });
+
+    // Setup the fake request object
+    const req = httpTestingController.expectOne('/api/courses/12');
+    // Expect the method to match
+    expect(req.request.method).toEqual('GET');
+    // Manually send the data for the subscribe function
+    req.flush(COURSES[12]);
+  });
+
+  afterEach(() => {
+    // Check that no unintended http requests are fired
+    httpTestingController.verify();
+  });
+
 });
