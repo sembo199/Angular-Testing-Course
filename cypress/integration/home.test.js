378b15a34d59a96d91862ca3ad600b5bbad20234
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2022-01-12 16:40:14 +0100

    38-mock-an-HTTP-backend-using-cypress

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,8 +1,20 @@
 describe('Home Page', () => {
   it('should display a list of courses', () => {
 
+    cy.fixture('courses.json').as('coursesJSON');
+
+    cy.server();
+
+    // @ sign to access the fixture
+    cy.route('/api/courses', "@coursesJSON").as('courses');
+
     cy.visit('/');
+
     cy.contains('All Courses');
+
+    cy.wait('@courses');
+
+    cy.get('mat-card').should("have.length", 9);
 
   });
 });
