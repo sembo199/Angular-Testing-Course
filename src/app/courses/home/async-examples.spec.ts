56b442919f7a0a9d4111f207e34413b1d579da0a
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2022-01-12 14:21:43 +0100

    28-fakeAsync-testing-zone-with-tick-and-flush

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,15 +1,39 @@
+import { fakeAsync, flush, tick } from "@angular/core/testing";
+
 fdescribe('Async Testing Examples', () => {
 
   it('async test example with jasmine done', (done: DoneFn) => {
     let test = false;
 
     setTimeout(() => {
-      console.log("Assertions executed");
+      console.log("Running assertions");
       test = true;
       expect(test).toBeTrue();
       done();
     }, 1000);
 
   });
+
+  it('async test example - setTimeout()', fakeAsync(() => {
+    let test = false;
+
+    setTimeout(() => {});
+
+    setTimeout(() => {
+      console.log("Running assertions setTimeout()");
+      test = true;
+    }, 1000);
+
+    // Tick controls evolution of time inside the fakeAsync code block
+    // tick(500);
+    // tick(499);
+    // tick(1);
+
+    // Flush will finish all async functions
+    flush();
+
+    // Would still work here
+    expect(test).toBeTrue();
+  }));
 
 });
